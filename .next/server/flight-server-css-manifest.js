d2882f5ad474d5e122e2630f95431e5ddb678b62
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2023-04-11 20:04:25 +0600

    first commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/mdjahidhasan/Documents/GitHub/My-First-Next-App/src/app/page.tsx": [
+      "/home/mdjahidhasan/Documents/GitHub/My-First-Next-App/node_modules/next/font/google/target.css?{\"arguments\":[{\"subsets\":[\"latin\"]}],\"import\":\"Inter\",\"path\":\"src/app/page.tsx\",\"variableName\":\"inter\"}"
+    ],
+    "/home/mdjahidhasan/Documents/GitHub/My-First-Next-App/src/app/layout.tsx": [
+      "/home/mdjahidhasan/Documents/GitHub/My-First-Next-App/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/home/mdjahidhasan/Documents/GitHub/My-First-Next-App/src/app/page": [
+      "/home/mdjahidhasan/Documents/GitHub/My-First-Next-App/node_modules/next/font/google/target.css?{\"arguments\":[{\"subsets\":[\"latin\"]}],\"import\":\"Inter\",\"path\":\"src/app/page.tsx\",\"variableName\":\"inter\"}",
+      "/home/mdjahidhasan/Documents/GitHub/My-First-Next-App/src/app/globals.css"
+    ]
+  }
+}
